86092bb7d0de20440c15d6ac257c354d65d0cc73
Pedro <pedrogarrido971@gmail.com>
2024-05-29 16:04:48 -0300

    cambie el nombre de la db

--- src/models/db.js
+++ src/models/db.js
@@ -3,7 +3,7 @@ import 'dotenv/config';
 
 const PASSWORD = process.env.PASSWORD
 
-const sequelize = new Sequelize('eternal-moments', 'root', PASSWORD, {
+const sequelize = new Sequelize('eternal_moments', 'root', PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
